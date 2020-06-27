29c6c9c926f14ccb5fdba90c0f1f5dd257e7047b
ca3zsar <cezartodirisca@gmail.com>
2020-06-28 00:40:34 +0300

    added basic interface

--- snake.py
+++ snake.py
@@ -24,17 +24,23 @@ WHITE = (255,255,255)
 pygame.init()
 windowSurface = pygame.display.set_mode((WIDTH,HEIGHT),0,32)
 pygame.display.set_caption("Snake")
+mainClock = pygame.time.Clock()
 
 mainFont = pygame.font.SysFont(None,30)
 
 # The snake class.
 class Snake(pygame.sprite.Sprite):
-    def __init__(self, startX, startY, surface):
-        super.__init__()
+    def __init__(self, startX, startY):
+        super().__init__()
         self.bodyX = [startX]
-        self.bodY = [startY]
+        self.bodyY = [startY]
         self.length = 1
         self.dir = STOP
+    def draw(self):
+        for i in range(self.length):
+            bodyRect = pygame.Rect(self.bodyX[i],self.bodyY[i],20,20)
+            pygame.draw.rect(windowSurface,GREEN,bodyRect)
+            
 
 def terminate():
     pygame.quit()
@@ -45,6 +51,12 @@ def waitForKey():
         for event in pygame.event.get():
             if event.type == QUIT:
                 terminate()
+            if event.type == KEYDOWN:
+                if event.key == K_RETURN:
+                    return
+                if event.key == K_ESCAPE:
+                    terminate()
+    
 
 def drawText(text, x, y,font):
     textobj = font.render(text, 1, GREEN)
@@ -52,16 +64,55 @@ def drawText(text, x, y,font):
     textrect.center = (x, y)
     windowSurface.blit(textobj, textrect)
 
-def Draw():
+def Draw(snake):
     windowSurface.fill(BLACK)
+    #Draw the borders of the game:
+    pygame.draw.line(windowSurface,WHITE,(50,50),(50,550))
+    pygame.draw.line(windowSurface,WHITE,(550,50),(550,550))
+    pygame.draw.line(windowSurface,WHITE,(50,50),(550,50))
+    pygame.draw.line(windowSurface,WHITE,(50,550),(550,550))
+    
+    #Draw the snake.
+    snake.draw()
+    
+    #Draw the score.
+    drawText(f"Score : {snake.length}",WIDTH//2,575,mainFont)
+    
+    pygame.display.update()
     
 
+def Input(snake):
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_UP or event.key == K_w:
+                snake.dir = UP
+                break
+            if event.key == K_DOWN or event.key == K_s:
+                snake.dir = DOWN
+                break
+            if event.key == K_LEFT or event.key == K_a:
+                snake.dir = LEFT
+                break
+            if event.key == K_RIGHT or event.key == K_d:
+                snake.dir = RIGHT
+                break
+            if event.key == K_ESCAPE:
+                terminate()
+        if event.type == QUIT:
+            terminate()
 
 def main():
     while True:
+        # Initialize the snake.
+        snake = Snake(WIDTH//2+10,HEIGHT//2+10)
         # Draw the "main menu" text and wait for user's input to continue or exit.
         windowSurface.fill(BLACK)
-        drawText("Press enter to continue",WIDTH/2,HEIGHT/2,mainFont)
+        drawText("Press enter to continue",WIDTH//2,HEIGHT//2,mainFont)
         pygame.display.update()
+        waitForKey()
+        while True:
+            Draw(snake)
+            Input(snake)
+            
         
 main()
